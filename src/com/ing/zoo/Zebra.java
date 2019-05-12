d52f770cf1add707e03c04cd7e58b7c3d5337514
Kwok Yee Chu <k.y.chu96@gmail.com>
2019-05-12 19:41:40 +0200

    [Added] initial project

---
+++ src/com/ing/zoo/Zebra.java
@@ -0,0 +1,26 @@
+package com.ing.zoo;
+
+import java.util.Random;
+
+public class Zebra {
+    public String name;
+    public String helloText;
+    public String eatText;
+    public String trick;
+
+    public Zebra()
+    {
+    }
+
+    public void sayHello()
+    {
+        helloText = "zebra zebra";
+        System.out.println(helloText);
+    }
+
+    public void eatLeaves()
+    {
+        eatText = "munch munch zank yee bra";
+        System.out.println(eatText);
+    }
+}
